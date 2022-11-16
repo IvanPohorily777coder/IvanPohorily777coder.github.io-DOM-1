10f92804b0d1b7dfa8851f6371653df893fa3789
IvanPohorily777coder <thepatrickmaps@gmail.com>
2022-11-16 18:08:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let headerLogo = document.getElementById('header__logo'),
+    lightButton = document.getElementById('light'),
+    temperatetButton = document.getElementById('temperate'),
+    darkButton = document.getElementById('dark'),
+    mainTitle = document.getElementById('main__title'),
+    mainDesc = document.getElementById('main__desc'),
+    footerTitle = document.getElementById('footer__title'),
+    bodyColor = document.body;
+
+
+
+    lightButton.onclick = function() {
+        bodyColor.style = "background-color: white; transition: 1s"; 
+        headerLogo.style = "color: black; transition: 2s";
+        lightButton.style = "color: black; transition: 2s";
+        temperatetButton.style = "color: black; transition: 2s";
+        darkButton.style = "color: black; transition: 2s";
+        mainTitle.style = "color: rgb(0, 0, 0); transition: 2s";
+        mainDesc.style = "color: rgb(0, 0, 0); transition: 2s";
+        footerTitle.style = "color: rgb(0, 0, 0); transition: 2s";
+    }
+
+    temperatetButton.onclick = function() {
+        bodyColor.style = "background-color: rgb(21,76,121); transition: 1s"; 
+        headerLogo.style = "color: white; transition: 2s";
+        lightButton.style = "color: white; transition: 2s";
+        temperatetButton.style = "color: white; transition: 2s";
+        darkButton.style = "color: white; transition: 2s";
+        mainTitle.style = "color: rgb(226,135,67); transition: 2s";
+        mainDesc.style = "color: rgb(226,135,67); transition: 2s";
+        footerTitle.style = "color: rgb(226,135,67); transition: 2s";
+    }
+
+    darkButton.onclick = function() {
+        document.location.reload();
+    }
+
+
+
+
+
